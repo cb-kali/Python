a842a1a1917f3b07133af67251c373db4d71954e
cb-kali <61385555+cb-kali@users.noreply.github.com>
2020-03-05 19:12:47 +0530

    Add files via upload

---
+++ Day2_B13.py
@@ -0,0 +1,28 @@
+# How to decaler variable and add int variable
+a = 10 # decaler
+b = 10 
+# add a and b int variable and store new variable
+c = a+b
+#print new variable ( a+b )
+print(c)
+
+# How to decaler variable in charter
+First_Name = "udit" # variable declar in small case
+Last_Name = "SHARMA" # variable declar in upper case
+# convert the first letter to capital
+print(First_Name) # check
+print(First_Name,title()) # convert 
+# convet the letter in capital
+print(First_Name) # check
+print(First_Name,upper()) # convert
+# convert the letter in small
+print(Last_Name) # check
+print(Last_Name,lower()) # convert
+
+# using 'f' String
+Full_Name = f'This is First name of candidate: {First_Name} and Last name: {Last_Name}'
+print(Full_Name)
+
+# convert the first letter in capital
+print(Full_Name,title())
+
