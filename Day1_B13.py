af2619895810247826c37d26487900cf8c462551
cb-kali <61385555+cb-kali@users.noreply.github.com>
2021-05-26 17:24:15 +0530

    Update Day1_B13.py

--- Day1_B13.py
+++ Day1_B13.py
@@ -1,1 +1,1 @@
-print("Hello I'm Chetan Bansal in this side , Thank you for this python fundamental  class ")
+print("Hello I'm Chetan Bansal in this side , Thank you for this python fundamental  class ")   # Fro print this 
